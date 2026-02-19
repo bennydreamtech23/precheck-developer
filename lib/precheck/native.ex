0e6b47a2f5560d8718fca0605d5234528e7b59d9
Uwabunkeonye Ijeoma <uwabunkeonyeijeoma@gmail.com>
2026-02-19 11:57:40 +0100

    updates on precheck_native.so

--- lib/precheck/native.ex
+++ lib/precheck/native.ex
@@ -5,9 +5,54 @@ defmodule Precheck.Native do
   All sensitive logic (including embedded scripts) lives in the Rust binary.
   This module provides the Elixir interface.
   """
-  use Rustler,
-    otp_app: :precheck,
-    crate: "precheck_native"
+
+  # Explicitly load the NIF from the new install path
+  @on_load :load_nif
+
+  def load_nif do
+    ext = nif_extension()
+    lib_name = "precheck_native#{ext}"
+
+    candidate_paths = [
+      "/usr/local/lib/precheck/priv/native/#{lib_name}",
+      Path.join([to_string(:code.priv_dir(:precheck)), "native", lib_name]),
+      Path.join([
+        Path.dirname(escript_path()),
+        "..",
+        "lib",
+        "precheck",
+        "priv",
+        "native",
+        lib_name
+      ])
+    ]
+
+    case Enum.find(candidate_paths, &File.exists?/1) do
+      nil ->
+        {:error, {:load_failed, "NIF library not found in known paths: #{Enum.join(candidate_paths, ", ")}"}}
+
+      nif_path ->
+        :erlang.load_nif(strip_extension(nif_path, ext), 0)
+    end
+  end
+
+  defp nif_extension do
+    case :os.type() do
+      {:unix, :darwin} -> ".dylib"
+      _ -> ".so"
+    end
+  end
+
+  defp escript_path do
+    case :escript.script_name() do
+      [] -> "."
+      charlist -> List.to_string(charlist)
+    end
+  end
+
+  defp strip_extension(path, ext) do
+    String.trim_trailing(path, ext)
+  end
 
   @doc "Run all security checks on a path"
   def run_checks(_path), do: :erlang.nif_error(:nif_not_loaded)
